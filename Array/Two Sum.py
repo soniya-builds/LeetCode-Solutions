c04f765b66ca1900bc188509f8e2df4ca5bbac8a
soniya-builds <soniyasmiles05@gmail.com>
2026-02-19 22:46:55 +0600

    Easy

--- Array/Two Sum.py
+++ Array/Two Sum.py
@@ -1,1 +1,11 @@
-#two sum
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+print(nums)
+target= int(input("Enter target: "))
+
+result = twoSum(nums, target)
+print("Indices are:", result)
